fe9876f32122ea234a900280b5f9979f428bad3b
morgankim <morgan022497@gmail.com>
2026-08-09 21:25:25 -0700

    0504 implemented unary eval

---
+++ 0504/os_unix.go
@@ -0,0 +1,32 @@
+//go:build unix
+
+package kv
+
+import (
+	"os"
+	"path"
+	"syscall"
+)
+
+// syncDir forces the OS to flush the directory metadata to the physical disk.
+func syncDir(file string) error {
+	flags := os.O_RDONLY | syscall.O_DIRECTORY 
+	dirfd, err := syscall.Open(path.Dir(file), flags, 0o644)
+	if err != nil {
+		return err 
+	}
+	defer syscall.Close(dirfd)
+	return syscall.Fsync(dirfd)
+}
+
+func createFileSync(file string) (*os.File, error) {
+	fp, err := os.OpenFile(file, os.O_RDWR | os.O_CREATE, 0o644)
+	if err != nil {
+		return nil, err
+	}
+	if err = syncDir(file); err != nil {
+		_ = fp.Close() // to avoid the memory leak.
+		return nil, err
+	}
+	return fp, err 
+}
